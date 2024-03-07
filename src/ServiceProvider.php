f9c98f375065c0b78dd8e82f27f66f8434b24ff2
Philipp Wahle <philipp.wahle@wahlemedia.de>
2024-03-07 22:44:53 +0100

    initial commit

---
+++ src/ServiceProvider.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Wahlemedia\StatamicMaintenanceMode;
+
+use Statamic\Providers\AddonServiceProvider;
+
+class ServiceProvider extends AddonServiceProvider
+{
+    public function bootAddon()
+    {
+        //
+    }
+}
